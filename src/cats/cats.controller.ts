2fac6f46ca24fab1de336e412075809b64801db9
muawiya <muawiyaaldanaf99@gmail.com>
2025-05-27 16:16:31 +0300

    try Provider stuff

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,27 +1,20 @@
-import { Controller, Get, Header, Param, Post, Redirect, Body, Query, HttpStatus, Res } from '@nestjs/common';
-import { CreateCatDto } from './create-cat.dto';
-import { Response } from 'express';
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
 
   @Post()
-  create(@Body() createCatDto: CreateCatDto, @Res() res: Response) {
-    res.status(HttpStatus.CREATED).send(`This action adds a new cat ${createCatDto.breed}`);
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
-  
+
   @Get()
-  async findAll(@Query('age') age: number, @Query('breed') breed: string, @Res({ passthrough: true }) res: Response) {
-    res.status(HttpStatus.OK);
-    return `This action returns all cats filtered by age: ${age} and breed: ${breed}`;
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
-
-  
-
-  @Get(':id')
-    findOne(@Param() params: any): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} cat`;
-}
-
 }
